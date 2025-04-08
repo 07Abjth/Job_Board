180a2f0d9144aad95892772b5f0a80266fe44227
Abhijith BS <vivekabi1717@gmail.com>
2025-04-08 21:21:43 +0530

    Frontend: React + DaisyUI setup, Tailwind config fixed, user layout & header added  UI: Started job application flow UI, integrated dark mode toggle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
    import './App.css'
    import React from 'react'
+   import { DarkMode } from './components/shared/DarkMode'
 
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
   wasupp
 </h1>
 
-<button className="btn btn-neutral">Neutral</button>
+<DarkMode/>
+ <button className="btn btn-neutral">Neutral</button>
 <button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
 <button className="btn btn-accent">Accent</button>
 <button className="btn btn-info">Info</button>
 <button className="btn btn-success">Success</button>
