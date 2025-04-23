8191b5275cdb50af9b827ecd62bab8f918506c28
Abhijith BS <vivekabi1717@gmail.com>
2025-04-23 21:24:43 +0530

    Fix import for axiosInstance

--- client/src/pages/user/JobList.jsx
+++ client/src/pages/user/JobList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
- import { axiosInstance } from '../../config/AxiosInstance';
+ import { axiosInstance } from '../../config/axiosInstance';
 
 export const JobList = () => {
   const [jobs, setJobs] = useState([]);
